1bdf6934db37d632fa499847c1350a0025f375f4
DanielMerser1 <91954962+M2101479@users.noreply.github.com>
2021-10-11 13:03:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+powers=input("Do they powers?")
+if powers== "Yes" or powers== "yes":
+  law=input("Have they ever killed?")
+  if law== "Yes" or law== "yes":
+    print("You have choose Red Hood")
+  else:
+    print("You have choosen Nightwing")
+else:
+  lives=input("Have they lived more then one life")
+  if lives=="Yes" or lives== "yes":
+    print("You have choosen Moiara")
+  else:
+    print("You have choosen Nightcrawler")
